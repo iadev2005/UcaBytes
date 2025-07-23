3be5a441b7b24251939c646c7c116dcdb3371c85
wildeswt <mfsandoval.23@est.ucab.edu.ve>
2025-07-23 16:17:52 -0400

    Revert "feat: primera base de home y carpetas pages, components"
    This reverts commit 39453170bdeef2d17436e74daa4484d5c405b940.

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App'
-import DesignSystem from './pages/DesignSystem'
-import Layout from './pages/Layout'
+import App from './App.tsx'
 
-function Main() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<App />} />
-          <Route path="design-system" element={<DesignSystem />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
-}
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Main />
-  </React.StrictMode>,
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
